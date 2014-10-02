3302722890731be2e03aa2ae9582b8a4d3f85391
Brian McEntee <brian.m.mcentee@gmail.com>
2014-10-02 19:25:24 -0400

    bad com

--- TriangleSums.java
+++ TriangleSums.java
@@ -1,7 +1,11 @@
 import java.util.Scanner;
+import java.util.Map;
+import java.util.HashMap;
 
 class TriangleSums {
-
+	//private static int[][] cellTable;
+	//private static Map<Integer,Integer> cache = new HashMap<Integer,Integer>();
+	private static int[][] cacheTable;
 	public static void main(String [] args) {
 		Scanner usrInput = new Scanner(System.in);
 		int numLines;
@@ -12,6 +16,8 @@ class TriangleSums {
 			numLines = usrInput.nextInt();
 			usrInput.nextLine();
 			triangle = new int[numLines][numLines];
+			//cellTable = generateKeyTable(numLines,numLines);
+			cacheTable = initCache(numLines,numLines);
 			for(int row = 0; row < numLines; row++) {
 				for(int col = 0; col <= row; col++) {
 					triangle[row][col] = usrInput.nextInt();
@@ -36,10 +42,43 @@ class TriangleSums {
 			return 0;
 		}
 		else{
+			if(cacheTable[row+1][col] != -1){
+				t1 = cacheTable[row+1][col];
+			}
+			else{
 			t1 = getGreatestSum(sumTriangle,row+1,col,len);
-			t2 = getGreatestSum(sumTriangle,row+1,col+1,len);
+			}
+			if(cacheTable[row+1][col+1] != -1){
+				t2 = cacheTable[row+1][col+1];
+			}
+			else{
+				t2 = getGreatestSum(sumTriangle,row+1,col+1,len);
+			}
 			t = Math.max(t1,t2) + sumTriangle[row][col];
+			cacheTable[row][col] = t;
 			return t;
 		}
+	}
+
+	// public static int[][] generateKeyTable(int row,int col) {
+	// 	int[][] keyTable = new int[row][col];
+	// 	int currKey = 1
+	// 	for(int i = 0; i< row; i++) {
+	// 		for(int j = 0; j < col; j++) {
+	// 			keyTable[i][j] = currKey;
+	// 			currKey++;
+	// 		}
+	// 	}
+	// 	return keyTable;
+	// }
+
+	public static int[][] initCache(int row,int col) {
+		int[][] cache = new int[row][col];
+		for(int i = 0; i< row; i++) {
+			for(int j = 0; j < col; j++) {
+				cache[i][j] = -1;
+			}
+		}
+		return cache;
 	}
 }
