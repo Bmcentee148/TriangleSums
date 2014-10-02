376534bcbbef1006e619d83cd4db4b8caad709dd
Brian McEntee <brian.m.mcentee@gmail.com>
2014-10-01 23:50:05 -0400

    cache finito

--- TriangleSums.java
+++ TriangleSums.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 
 class TriangleSums {
-
+	private static int[][] cache;
+	private static int testCount = 0;
 	public static void main(String [] args) {
 		Scanner usrInput = new Scanner(System.in);
 		int numLines;
@@ -12,6 +13,7 @@ class TriangleSums {
 			numLines = usrInput.nextInt();
 			usrInput.nextLine();
 			triangle = new int[numLines][numLines];
+			cache = initCache(numLines);
 			for(int row = 0; row < numLines; row++) {
 				for(int col = 0; col <= row; col++) {
 					triangle[row][col] = usrInput.nextInt();
@@ -22,7 +24,7 @@ class TriangleSums {
 		for(int i = 0; i < solutions.length; i++) {
 			System.out.println(solutions[i]);
 		}
-
+		System.out.println(testCount);
 	}
 
 	public static int getGreatestSum(int[][] sumTriangle) {
@@ -35,11 +37,28 @@ class TriangleSums {
 		if(row == len) {
 			return 0;
 		}
+		else if(cache[row][col]!= -1){
+			testCount++;
+			return cache[row][col];
+		}
 		else{
 			t1 = getGreatestSum(sumTriangle,row+1,col,len);
 			t2 = getGreatestSum(sumTriangle,row+1,col+1,len);
 			t = Math.max(t1,t2) + sumTriangle[row][col];
+			cache[row][col] = t;
 			return t;
 		}
 	}
+
+	private static int[][] initCache(int len) {
+		//The grid we need will always be a square
+		int [][] r_cache = new int[len][len];
+		for(int i = 0; i< len; i++) {
+			for(int j = 0; j < len; j++) {
+				r_cache[i][j] = -1; //to signify its empty
+			}
+		}
+		return r_cache;
+	}//end initCache
+
 }
